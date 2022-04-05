7ea8ab5041f04235ecc0361277c0fb71f07605e9
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-04-04 20:36:26 -0600

    Added a flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
     def create 
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
+            flash[:notice] = "The article has been created successfully."
             redirect_to @article
         else 
             render 'new'
