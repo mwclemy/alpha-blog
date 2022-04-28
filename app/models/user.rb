afd8488244019c828534827e06b0b3f569da3fd8
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-04-27 20:49:59 -0600

    added password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
     validates :email, presence: true, 
                       length: {maximum: 105}, 
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    has_secure_password
 end
