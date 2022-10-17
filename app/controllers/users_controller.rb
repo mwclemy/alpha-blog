48e338702d0815d55d0732ed6c6d5cfb129d748e
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-10-16 22:45:49 -0600

    added restrictions on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    helper_method :current_user
+    before_action :set_user, only:[:show, :edit, :update]
+    before_action :require_user, only:[:edit, :update]
+    before_action :require_same_user, only:[:edit, :update]
+
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -19,16 +24,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update 
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user
@@ -38,8 +40,16 @@ class UsersController < ApplicationController
     end
 
     private
-    
+    def set_user
+        @user = User.find(params[:id])
+    end
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    def require_same_user 
+        if current_user != @user
+            flash[:alert] = "You can only edit your profile"
+            redirect_to @user
+        end
     end
 end
