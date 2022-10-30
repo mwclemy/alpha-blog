356a21f3f0724ae5cdb61ccbb27ce6dbd451ebe5
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-10-30 16:25:18 -0600

    added categories listing

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create a category" do
+    get "/categories/new"
+    assert_response :success
+
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: "Travel" } }
+      assert_response :redirect
+    end
+
+    follow_redirect!
+    assert_response :success
+    assert_match "Travel", response.body
+  end
+
+  test "get new category form and test invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+
+    assert_match "error", response.body
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
+  end
+end
